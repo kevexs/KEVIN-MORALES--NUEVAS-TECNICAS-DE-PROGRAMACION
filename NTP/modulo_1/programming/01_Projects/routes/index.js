721f52fd3f1a293728720b8fa3d1121f11a08872
kevexs <kevinwladimir20@gmail.com>
2021-06-24 18:27:34 -0500

    actividad en clase

---
+++ NTP/modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,19 @@
+//traemos el paquete
+const { Router } = require('express');
+
+//inicializamos ROUTER
+const router = Router()
+
+//Creacion de los endpoint
+
+//Exportaos los rutas
+module.exports.RouterIndex = router;
+/*
+
+module = {
+    exports: {
+        RouterIndex: router
+    }
+}
+*/
+
