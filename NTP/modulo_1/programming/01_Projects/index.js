e5daaf330d2b4f26bbce2d44c72646caba43d3de
kevexs <kevinwladimir20@gmail.com>
2021-06-25 18:34:32 -0500

    actividad en clases-ROUTER

--- NTP/modulo_1/programming/01_Projects/index.js
+++ NTP/modulo_1/programming/01_Projects/index.js
@@ -1,12 +1,14 @@
+const { Router } = require('express');
 const express = require('express');
-
 //Genero una aplicacion express
 const app = express()
+//router
+const {RouterIndex}=require('routes/index')
 
-app.get('/', (req, res) => {
-      res.send("Hola mundo con ExpressJs")
-})
-
+    app.user('/RouterIndex')
+    //app.user('/user'. RouterIndex)
+    //app.user('/user/data'. RouterIndex)
+    //app.user('/user'. RouterIndex)
 app.listen(3000, () => {
     console.log("Servidor escuchando en http://localhost:3000");
     
--- NTP/modulo_1/programming/01_Projects/routes/index.js
+++ NTP/modulo_1/programming/01_Projects/routes/index.js
@@ -4,16 +4,39 @@ const { Router } = require('express');
 //inicializamos ROUTER
 const router = Router()
 
-//Creacion de los endpoint
+//traer data
+const data = require('../data.json')
+//creacion de los entpoint
+router.get("/", (req, res =>{
+    res.send("Hola mundo conexpress ")
+}))
 
-//Exportaos los rutas
+
+.get("/users", (req, res) =>{
+    res.json({
+        msg:"Lista de usuarios body:data"
+    })
+})
+get("/saludo", (req, res) =>{
+    const{
+        query: {nombre,
+        apellido},
+    } = req;
+    res.json({
+        saludo: "mundo",
+        nombre: nombre,
+        apellido: apellido,
+    });
+    })
+.get("/saludo/:nombre",(req, res) =>{
+    const{params: {nombre}
+}=resq;
+res.json({
+    saludo: "mundo",
+    nombre: nombre,
+})
+
+})
+// Exportamos rutas
 module.exports.RouterIndex = router;
-/*
-
-module = {
-    exports: {
-        RouterIndex: router
-    }
-}
-*/
-
+/
